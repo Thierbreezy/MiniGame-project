312566e89ccfe88335ba5929f74ea24033498eb6
Thierbreezy <thiernosbdiallo@gmail.com>
2026-07-07 11:26:57 +0000

    MiniGame-project

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,83 @@
+import { 
+    View, 
+    StyleSheet, 
+    Image, 
+    Text, 
+    useWindowDimensions,
+    ScrollView
+ } from 'react-native';
+
+import Title from '../components/ui/Title';
+import PrimaryButton from '../components/ui/PrimaryButton';
+import Colors from '../constants/Colors';
+
+function GameOverScreen({ roundNumber, userNumber, onStartNewGame }) {
+
+    const { width, height } = useWindowDimensions();
+
+    let imageSize = 300;
+
+    if (width < 380) {
+        imageSize = 150;
+    }
+    if (height < 400) {
+        imageSize = 80.
+    }
+
+    const imageStyle = {
+        width: imageSize,
+        height: imageSize,
+        borderRadius: imageSize / 2
+    }
+
+    return (
+    <ScrollView style={styles.screen}>
+        <View style={styles.rootContainer}>
+            <Title>Game Over!</Title>
+            <View style={[styles.imageContainer, imageStyle]}>
+            <Image source={require('../assets/success.png')} style={styles.image} />
+            </View>
+            <Text style={styles.summaryText}>
+                Your phone needed <Text style={styles.highlight}> {roundNumber} </Text>  
+                rounds to guess the number
+                <Text style={styles.highlight}> {userNumber} </Text>.
+            </Text>
+            <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
+        </View>
+    </ScrollView>
+);
+}
+
+export default GameOverScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1
+    },
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageContainer: {
+        borderWidth: 3,
+        borderColor: Colors.primary800,
+        overflow: 'hidden',
+        margin: 36,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+    summaryText: {
+        fontFamily: 'open-sans',
+        fontSize: 24,
+        textAlign: 'center',
+        marginBottom: 24,
+    },
+    highlight: {
+        fontFamily: 'open-sans-bold',
+        color: Colors.primary500,
+    },
+});
